90c5b56ae8dc688e237fc0a429a09401a9b3103d
Shann Stephen Enciso <Shann.enciso@gmail.com>
2024-11-22 22:00:16 +0800

    Update

--- Assets/Scripts/Player Scripts/playerCollision.cs
+++ Assets/Scripts/Player Scripts/playerCollision.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class monsterTrigger : MonoBehaviour
 {
+#region Fields and Properties
+
+    //GAME OBJECT
+    [SerializeField] public GameObject GoblinFight;
+    //SCRIPT
     playerMovement plyMS;
 
-    #region Player Collision
+#endregion
+
+#region Start and Update
+
+
+#endregion
+
+
+#region Player Collision
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject.name);
 
-        //miniBoss.fightPanelScene();
-
-        if (collision.gameObject.name == "enemy1")
+        if (collision.gameObject.name == "Goblin")
         {
             Debug.Log("First Monster Hit");
+
+            SceneManager.LoadSceneAsync("scn BATTLE");
+            
             
         }
     }
 
-    #endregion
+#endregion
 }
